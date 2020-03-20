9483996b099fc43efcebbcc2068fd7affc33fb51
Steffi Große-Bölting <sgrosseboelting@gmail.com>
2020-03-20 21:26:46 +0100

    Pages contact and about

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText={"About Gatsby"} />
+    <Header headerText={"That's pretty cool"} />
+    <p>Such wow. Very React.</p>
+  </div>
+)
